ac1d15afbce0f51f863bb6ed5344dd6aaf959c7c
Daniel Xie <xiedaniel@outlook.com>
2018-08-21 23:49:50 +0800

    add a little basic JavaScript

---
+++ Day5-6/scripts/script.js
@@ -0,0 +1,7 @@
+document.getElementById("contract-btn").onclick = function() {
+    alert("我的联系方式都在简历上啦");
+}
+document.getElementById("hobby-btn").onclick = function() {
+    console.log("Diving")
+    alert("我已经告诉你我的特殊爱好啦，找找看");
+}
